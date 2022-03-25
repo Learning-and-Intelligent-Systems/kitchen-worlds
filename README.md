bf0848c6ea99b2c4b77f1f16c2b7ad1ba9e0ce43
ztyang <ztyang@mit.edu>
2022-03-25 13:55:01 -0400

    basic scene

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # kitchen-worlds
+
+![example kitchen scene](media/move_egg.png)
+
+A collection of kitchen scenes in LISDF format (an extension to SDF that includes URDF), kitchen object models, and TAMP problems. A product of LIS TAMP I/O Hackathon 03/25/2022.
+
+## Setup
+
+Initiate lisdf submodule:
+
+```shell
+git submodule init 
+```
+
+## Examples
+
+To test parsing of LISDF files, run
+
+```shell
+python parser_test.py
+```
+
+To test loading LISDF worlds into PyBullet, run
+
+```shell
+python pybullet_test.py
+```
+
+## TODO
+
+- [ ] upload test scene files in the format of `.lisdf` -> need lisdf team to resolve a few issues with parsing
+- [ ] upload problem files in the format of `problem.pddl`
+- [ ] add `requirements.txt`
+- [ ] upload implicit domain files in the format of `domain.pddl`
+- [ ] add instructions to solve the example problems with TAMP planner [PDDLStream](https://github.com/caelan/pddlstream/tree/main)
+- [ ] add instructions on generating kitchen scenes with scripts in [cognitive-architectures](https://github.mit.edu/ztyang/cognitive-architectures/tree/master/bullet)
+
+## Acknowledgements
+
+In order to be lightweight, some functions in this repo is copied from the work of other LIS members instead of added as part of their original submodules:
+
+* the [lisdf](https://github.com/Learning-and-Intelligent-Systems/lisdf) package is developed by William Shen, Nishanth Kumar, Aidan Curtis, and Jiayuan Mao
+* the [pybullet_tools](https://github.com/caelan/pybullet-planning/tree/master/pybullet_tools) package is a subset of tools developed by Caelan Garret
+* most object models are downloaded from [PartNet Mobility dataset](https://sapien.ucsd.edu/browse) (Mo, Kaichun, et al. "Partnet: A large-scale benchmark for fine-grained and hierarchical part-level 3d object understanding." *Proceedings of the IEEE/CVF conference on computer vision and pattern recognition*. 2019.)
---
+++ pybullet_lisdf.py
@@ -0,0 +1,67 @@
+import os
+import sys
+from os.path import join, abspath, dirname, isdir, isfile
+sys.path.append('lisdf')
+from lisdf.parsing.sdf_j import load_sdf
+from lisdf.components.model import URDFModel
+
+def make_sdf_world(sdf_model):
+    """ temporary fix for LISDF format """
+    sdf_model = sdf_model.replace(',', '')
+    last_line = ''
+    sdf_model_fixed = ''
+    for line in sdf_model.split('\n'):
+        if not ('<geometry>' in last_line and '<pose>' in line) and not 'visual>' in line:
+            sdf_model_fixed += line + '\n'
+        last_line = line
+
+    return f"""<?xml version="1.0" ?>
+<!-- tmp sdf file generated from LISDF -->
+<sdf version="1.9">
+  <world name="tmp_world">
+
+{sdf_model_fixed}
+
+  </world>
+</sdf>"""
+
+def load_lisdf_pybullet(sdf_path):
+    from pybullet_tools.utils import load_pybullet, connect, wait_if_gui, \
+        disconnect, HideOutput
+
+    tmp_path = join('assets', 'tmp')
+
+    connect(use_gui=True, shadows=False, width=1980, height=1238)
+
+    # with HideOutput():
+        # load_pybullet(join('models', 'Basin', '102379', 'mobility.urdf'))
+    # load_pybullet(sdf_path)  ## failed
+    # load_pybullet(join(tmp_path, 'table#1_1.sdf'))
+
+    world = load_sdf(sdf_path).worlds[0]
+    for model in world.models:
+        if isinstance(model, URDFModel):
+            uri = model.uri
+        else:
+            uri = join(tmp_path, f'{model.name}.sdf')
+            with open(uri, 'w') as f:
+                f.write(make_sdf_world(model.to_sdf()))
+
+        # with HideOutput():
+
+        body = load_pybullet(uri)
+
+        # if tmp_path in uri:
+        #     os.remove(tmp_path)
+
+        wait_if_gui('load next model?')
+
+    wait_if_gui('exit pybullet?')
+    disconnect()
+
+
+if __name__ == "__main__":
+
+    for sdf_test in ['counter']: ## 'm0m_test' ## "kitchen_test.sdf"
+        sdf_path = join('assets', 'scenes', f'{sdf_test}.lisdf')
+        world = load_lisdf_pybullet(sdf_path)
