43f60d796e0dece93f483705eb2991a9544cf8ba
Zhutian Yang <zhutiany@nvidia.com>
2022-08-02 23:02:41 -0700

    render occluded cabbage

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -31,68 +31,97 @@ def render_rgb_image(test_dir, viz_dir, camera_pose):
 
 def render_segmented_rgb_images(test_dir, viz_dir, camera_pose, robot=False):
     get_depth_images(test_dir, camera_pose=camera_pose, rgb=True, robot=robot,
-                     img_dir=join(viz_dir, 'rgb'))
-
-
-def render_segmented_depth_images(test_dir, viz_dir, camera_pose, robot=False):
-    get_depth_images(test_dir, camera_pose=camera_pose, rgb=False, robot=robot,
-                     img_dir=join(viz_dir, 'depth'))
+                     img_dir=join(viz_dir, 'rgb_images'))
 
 
 def render_segmented_rgbd_images(test_dir, viz_dir, camera_pose, robot=False):
-    get_depth_images(test_dir, camera_pose=camera_pose, rgb=True, d=True, robot=robot,
-                     img_dir=join(viz_dir, 'depth'))
+    get_depth_images(test_dir, camera_pose=camera_pose, rgbd=True, robot=robot,
+                     img_dir=join(viz_dir))
 
 
 def render_masked_rgb_images(viz_dir):
     import numpy as np
 
-    out_dir = join(viz_dir, 'masked_rgb')
-    os.mkdir(out_dir)
-    crops = { 'rgb': (50, 50, 562, 413), 'depth': (49, 30, 446, 398) }
+    new_key = 'masked_rgb'
+    out_dir = join(viz_dir, f"{new_key}s")
+    if not isdir(out_dir): os.mkdir(out_dir)
+
+    # crops = { 'rgb': (50, 50, 562, 413), 'depth': (49, 30, 446, 398) }
 
     def load_rgbd(rgb_image_name):
-        depth_image_name = rgb_image_name.replace('rgb_image', 'depth_map')
-        rgb_img = np.asarry(PIL.Image.open(rgb_image_name).crop(crops['rgb']))
-        depth_img = np.asarry(PIL.Image.open(depth_image_name).crop(crops['depth']))
+        depth_image_name = rgb_image_name.replace('rgb', 'depth')
+        rgb_img = np.asarray(PIL.Image.open(rgb_image_name).convert('RGB')) ## .crop(crops['rgb']))
+        depth_img = np.asarray(PIL.Image.open(depth_image_name)) ## .crop(crops['rgb']))
         return rgb_img, depth_img
 
-    def mask_from_image(arr):
-        print()
-        return arr
+    def expand_mask(mask):
+        y = np.expand_dims(mask, axis=2)
+        return np.concatenate((y, y, y), axis=2)
+
+    def mask_from_rgb_image(arr):
+        background = make_image_background(arr)
+        i = np.where(np.sum(np.abs(arr - background), axis=2) > 5)
+        mask = np.zeros_like(arr[:, :, 0])
+        mask[i] = 1
+        return expand_mask(mask)
+
+    def mask_from_depth_image(arr):
+        i = np.where(arr != 0)
+        mask = np.zeros_like(arr)
+        mask[i] = 1
+        return mask
+
+    def get_visibility_mask(obj, scene, mask):
+        visibility_mask = np.zeros_like(obj)
+        i = np.where(obj*mask == scene*mask)
+        visibility_mask[i] = 1
+        return expand_mask(visibility_mask*mask)
+
+    def make_image_background(old_arr):
+        new_arr = np.ones_like(old_arr)
+        new_arr[:, :, 0] = 178
+        new_arr[:, :, 1] = 178
+        new_arr[:, :, 2] = 204
+        return new_arr
 
     ## ----- step 1: read the instance rgb and depth images
-    rgb_dir = join(viz_dir, 'rgb')
+    rgb_dir = join(viz_dir, 'rgbs')
     rgb_files = [f for f in listdir(rgb_dir)]
     rgb_scene = [join(rgb_dir, f) for f in rgb_files if 'scene' in f][0]
     rgb_scene_img, depth_scene_img = load_rgbd(rgb_scene)
+    shutil.copy(rgb_scene, rgb_scene.replace('rgb', new_key))
 
     ## ----- step 2: render the masked out version of the scene rgb if the depth values are the same
+    rgb_files.sort()
     for f in rgb_files:
         if 'scene' in f: continue
         rgb_obj = join(rgb_dir, f)
         rgb_obj_img, depth_obj_img = load_rgbd(rgb_obj)
 
-        rgb_mask = mask_from_image(rgb_obj_img)
-        depth_mask = mask_from_image(depth_obj_img)
+        rgb_mask = mask_from_rgb_image(rgb_obj_img)
+        depth_mask = mask_from_depth_image(depth_obj_img)
 
-        depth_ray_mask = depth_mask * depth_obj_img == depth_mask * depth_scene_img
-        masked_rgb_obj = 0 ## background color
+        visibility_mask = get_visibility_mask(depth_obj_img, depth_scene_img, depth_mask)
 
-        masked_rgb_obj += rgb_mask * rgb_scene
-        im = PIL.Image.fromarray(masked_rgb_obj)
-        im.save("your_file.jpeg")
+        background = make_image_background(rgb_obj_img)  ## background color [178, 178, 204] in png image
+        foreground = np.zeros_like(background) + rgb_scene_img * rgb_mask * visibility_mask
+        background[np.where(foreground!=0)] = 0
+        background += foreground
+        im = PIL.Image.fromarray(background)
+        im.save(rgb_obj.replace('rgb', new_key))
 
 
 if __name__ == "__main__":
-    # dataset_dir = '/home/zhutiany/Documents/mamao-data/one_fridge_pick_pr2'
-    dataset_dir = '/Users/z/Documents/simulators/PyBullet/kitchen-worlds/outputs/one_fridge_pick_pr2'
+    dataset_dir = '/home/zhutiany/Documents/mamao-data/one_fridge_pick_pr2'
+    # dataset_dir = '/Users/z/Documents/simulators/PyBullet/kitchen-worlds/outputs/one_fridge_pick_pr2'
     task_name = dataset_dir[dataset_dir.rfind('/')+1:]
-    for subdir in listdir(dataset_dir):
+    subdirs = listdir(dataset_dir)
+    subdirs.sort()
+    for subdir in subdirs:
         if not isdir(join(dataset_dir, subdir)): continue
         viz_dir = join(dataset_dir, subdir)
 
-        if not isdir(join(viz_dir, 'rgb')):
+        if not isdir(join(viz_dir, 'rgbs')):
             camera_pose = get_camera_pose(viz_dir)
 
             ## need to temporarily move the dir to the test_cases folder for asset paths to be found
@@ -102,17 +131,16 @@ if __name__ == "__main__":
 
             ## ------------- visualization function to test -------------------
             # render_rgb_image(test_dir, viz_dir, camera_pose)
-            if not isdir(join(viz_dir, 'rgb_images')):
-                render_segmented_rgb_images(test_dir, viz_dir, camera_pose, robot=True)
-            # render_segmented_rgb_images(test_dir, viz_dir, camera_pose)
-            # render_segmented_depth_images(test_dir, viz_dir, camera_pose)
+            # if not isdir(join(viz_dir, 'rgb_images')):
+            #     render_segmented_rgb_images(test_dir, viz_dir, camera_pose, robot=True)
+
+            ## without robot
             render_segmented_rgbd_images(test_dir, viz_dir, camera_pose)
 
             shutil.rmtree(test_dir)
             reset_simulation()
 
-        render_masked_rgb_images(viz_dir)
+        print(viz_dir)
+        if not isdir(join(viz_dir, 'masked_rgbs')):
+            render_masked_rgb_images(viz_dir)
         ## ----------------------------------------------------------------
-
-        break
-
