90ff682e913646c7656b5ecfae24412df8e71757
Weiyu Liu <wliu88@gatech.edu>
2023-05-08 07:12:23 -0700

    add gym env

---
+++ tests/test_gym_api.py
@@ -0,0 +1,12 @@
+import gymnasium as gym
+env = gym.make("CartPole-v1")
+
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    print(observation)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+env.close()
