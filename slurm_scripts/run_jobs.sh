5e7e7afdee87ed77ac9d0381527dcf557ed31796
Weiyu Liu <wliu88@gatech.edu>
2023-09-24 00:15:40 -0700

    fix

--- slurm_scripts/run_jobs.sh
+++ slurm_scripts/run_jobs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 0 99 0 10'
-sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 100 199 0 10'
-sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 200 299 0 10'
-sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 300 399 0 10'
-sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 400 499 0 10'
-sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 500 599 0 10'
-sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 600 699 0 10'
+# sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 100 199 0 10'
+# sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 200 299 0 10'
+# sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 300 399 0 10'
+# sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 400 499 0 10'
+# sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 500 599 0 10'
+# sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 600 699 0 10'
 
 sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts_parallel.sh'
