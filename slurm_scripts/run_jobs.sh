5a8c7b678c643d164bf7c1ec5eefee6627154cc4
Weiyu Liu <wliu88@gatech.edu>
2023-09-23 23:59:08 -0700

    collect more

--- slurm_scripts/run_jobs.sh
+++ slurm_scripts/run_jobs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 0 199 0 10'
-sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 200 399 0 10'
-sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 400 599 0 10'
-sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 600 799 0 10'
+sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 0 99 0 10'
+sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 100 199 0 10'
+sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 200 299 0 10'
+sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 300 399 0 10'
+sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 400 499 0 10'
+sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 500 599 0 10'
+sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts.sh 600 699 0 10'
 
 sbatch start_sbatch_job.sh 'bash bash_scripts/cluster/collect_rollouts_parallel.sh'
