fd25b944264418920506c7146e2b97ede1c61903
Yang <zhutiany@nvidia.com>
2024-05-23 21:37:55 -0400

    kitchen world bug fix

--- examples/test_data_generation_pigi.py
+++ examples/test_data_generation_pigi.py
@@ -5,7 +5,10 @@ from cogarch_tools.processes.pddlstream_agent import PDDLStreamAgent
 
 
 if __name__ == '__main__':
+    seed = 378277
+    goal_variations = [3]
     run_agent(
         agent_class=PDDLStreamAgent, config='config_pigi.yaml',
+        goal_variations=goal_variations, seed=seed
     )
 
